6ddfde676f5ff38f6d728206a14b523360ea6995
BrunoPasca <42937086+BrunoPasca@users.noreply.github.com>
2018-10-12 11:54:50 -0300

    Araña

--- Assets/Scripts/Enemies/EnemyDamage.cs
+++ Assets/Scripts/Enemies/EnemyDamage.cs
@@ -59,7 +59,7 @@ public class EnemyDamage : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-
+        
 
         distance = Vector3.Distance(firePoint.position, player.position);
         //head.transform.localRotation.x 
@@ -79,6 +79,19 @@ public class EnemyDamage : MonoBehaviour {
 
             casted = false;
 
+            
+        }
+        if (time2 > Cooldown)
+        {
+            line.SetPosition(0, Vector3.zero);
+            line.SetPosition(1, Vector3.zero);
+            attack.SetPosition(0, Vector3.zero);
+            attack.SetPosition(1, Vector3.zero);
+
+            casted = false;
+            time1 = 0;
+            time2 = 0;
+            attacking = false;
         }
     }
 
@@ -91,13 +104,14 @@ public class EnemyDamage : MonoBehaviour {
     public void Attack()
     {
         time1++;
+        time2++;
         hitPoint += (player.position - hitPoint) * speed * Time.deltaTime;
 
         movement.canMove = true;
-        Debug.Log("Movement.StopMoving");
-        anim.enabled = false;
-      
         
+        anim.enabled = false;
+
+
         if (!attacking)
         {
             //Instantiate(linePrefab,firePoint.position,Quaternion.identity);
@@ -111,6 +125,7 @@ public class EnemyDamage : MonoBehaviour {
             Debug.Log(distanceToHit);
             if (distanceToHit < .5f)
             {
+                Debug.Log(time2);
                 attacking = true;
                 CastRay();
             }
@@ -124,21 +139,23 @@ public class EnemyDamage : MonoBehaviour {
         //Instantiate(rayPrefab, firePoint.position, Quaternion.identity);
         attack.SetPosition(0, firePoint.position);
         attack.SetPosition(1, hitPoint);
-
+        
         line.SetPosition(0, Vector3.zero);
         line.SetPosition(1, Vector3.zero);
         casted = true;
         time1 = 0;
         attacking = false;
+
+        
         if (attack.GetPosition(1) == lastPos.position)
         {
             
-            Debug.Log("Disparing Player");
+            
             playerHealth.TakeDamage(enemyDamage);
 
         } else
         {
-            Debug.Log("Attack Missed");
+            
         }
     }
 }
