a4ef186d2495ebe60c26febe31173a2b70b47a4d
Supernico12 <37745277+Supernico12@users.noreply.github.com>
2018-10-22 10:05:08 -0300

    fsd
    HA

--- Assets/Scripts/CharacterS/CharacterStats.cs
+++ Assets/Scripts/CharacterS/CharacterStats.cs
@@ -6,44 +6,18 @@ public class CharacterStats : MonoBehaviour
 {
 
 
-<<<<<<< HEAD
     [SerializeField]
     float health;
-=======
-	[SerializeField]
-	float health;
-    
->>>>>>> 2b16ca68d2adda29811d496a53c094b3bed07da9
 
 
     float currenthealth;
     [SerializeField] GameObject ammoDrop;
 
-    Animator anim;
-
-<<<<<<< HEAD
 
 
 
 
-=======
-    
 
-
-
-    
-	public void TakeDamage(float damage)
-	{
-		currenthealth -= damage;
-		if (currenthealth <= 0)
-		{
-            
-            if (anim != null)
-                Debug.Log("Die" + anim.GetBool("Die"));
-                anim.SetBool("Die", true);
-        }
-	}
->>>>>>> 2b16ca68d2adda29811d496a53c094b3bed07da9
 
     public void TakeDamage(float damage)
     {
@@ -55,25 +29,9 @@ public class CharacterStats : MonoBehaviour
         }
     }
 
-<<<<<<< HEAD
 
     public virtual void Die()
     {
-=======
-	public virtual void Die()
-	{
-        
-
-		Instantiate(ammoDrop,transform.position,Quaternion.identity);
-		Destroy(gameObject);
-	}
-
-	void Start()
-	{
-         
-        anim = GetComponent<Animator>();
-		currenthealth = health;
->>>>>>> 2b16ca68d2adda29811d496a53c094b3bed07da9
 
         Instantiate(ammoDrop, transform.position, Quaternion.identity);
         Destroy(gameObject);
