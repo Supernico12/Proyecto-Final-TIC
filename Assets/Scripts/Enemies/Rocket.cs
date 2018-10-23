a3701de2ad648ec4a0808b70860ab3b206730260
Supernico12 <37745277+Supernico12@users.noreply.github.com>
2018-10-23 15:52:05 -0300

    Revert "Merge branch 'master' of https://github.com/Supernico12/Proyecto-Final-TIC"
    This reverts commit 04ca1f25b90f5820abf02633497755d50e33e7cc, reversing
    changes made to ed42726b89dc2c973be2575f2ec9d4f6f0f5e6c4.

--- Assets/Scripts/Enemies/Rocket.cs
+++ Assets/Scripts/Enemies/Rocket.cs
@@ -6,52 +6,14 @@ public class Rocket : MonoBehaviour {
     public float BulletSpeed;
 
     Vector3 targeteano;
-    public GameObject player;
+    Transform player;
     Rigidbody rb;
     public int timebeforedestroy;
     int time;
-
-    public float damage;
-
-    public ParticleSystem explosion;
-
-    CharacterStats character;
+    
 
     void Start()
     {
-<<<<<<< HEAD
-        player = PlayerManager.instance.player;
-
-        character = player.GetComponent<CharacterStats>();
-        
-        
-
-        
-
-    }
-    void FixedUpdate()
-    {
-
-
-        transform.LookAt(player.transform.position);
-
-        Debug.Log("Distance" + Vector3.Distance(transform.position, player.transform.position));
-       
-    }
-    void Update()
-    {
-
-        transform.position += transform.forward * BulletSpeed * Time.deltaTime;
-
-        if (Vector3.Distance(transform.position, player.transform.position) <= 4f)
-        {
-            Debug.Log("Destroying");
-            //character.TakeDamage(damage);
-            Instantiate(explosion, transform.position, Quaternion.identity);
-            Destroy(gameObject);
-        }
-
-=======
         rb = gameObject.GetComponent<Rigidbody>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
         
@@ -68,7 +30,6 @@ public class Rocket : MonoBehaviour {
     void Update()
     {
         
->>>>>>> 3432a3a2b291b935617229450ee0a44eeda93337
         time++;
         if (time > timebeforedestroy)
         {
