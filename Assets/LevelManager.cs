b855d7dbe6b7e5a585c617cbb7d4436a0a1f3778
BrunoPasca <42937086+BrunoPasca@users.noreply.github.com>
2018-10-29 08:13:12 -0300

    Revert "IntentoDeNoRomperTodo1.0"
    This reverts commit 9d9fdac25f90100e021e2c923600ee2c60cf2714, reversing
    changes made to d6a3bd93b69dd588ecd55090d2d9d83f67936e0f.

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour {
 
-    int enemyCount;
-
 	// Use this for initialization
 	void Start () {
 		
@@ -14,20 +12,5 @@ public class LevelManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-        if (enemyCount <= 0)
-        {
-
-        }
-
 	}
-    public void AddEnemy(int cantEnemy)
-    {
-        enemyCount += cantEnemy;
-    }
-
-    public void RemoveEnemy(int cantEnemy)
-    {
-        enemyCount -= cantEnemy;
-    }
-
 }
